9d838f5e6f787ae0cfe63387087a94d640a9d759
esun <esun@expediagroup.com>
2022-05-08 22:04:09 -0700

    Understanding beforeAll and afterAll

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,13 +1,17 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeClass(){
+        System.out.println("Before-I am only called once!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -21,7 +25,22 @@ class GreetingTest {
     }
 
     @Test
+    void helloSam() {
+        System.out.println(greeting.helloWorld("Sam!"));
+    }
+
+    @Test
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("Edward"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("TearDown executing...");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After everything !- I am only called once in entire test suite!");
     }
 }
